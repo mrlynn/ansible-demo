0c79802832f51b75c45d61eedc24c91606668577
Michael Lynn <merlynn@gmail.com>
2016-10-06 16:44:22 -0400

    add nocows explanation

--- 1_ansible-cfg.sh
+++ 1_ansible-cfg.sh
@@ -14,10 +14,14 @@ more -N ansible.cfg
 echo ${reset}
 echo Notice the following line:
 echo     ${green}private_key_file = /Users/mlynn/.ssh/michael.lynn.pem${reset}
+echo "This is the ${red}secret${reset} to how ansible can get to all my hosts."
 echo
 echo "The ${bold}ansible.cfg${reset} file can be stored anywhere on your master configuration management node.  You can tell ansible where to find its configuration file using the environment variable 'ANSIBLE_CONFIG'."
 echo
-echo "This is the ${red}secret${reset} to how ansible can get to all my hosts."
+echo "What's nocows?"
+pause
+cowsay "cowsay is a program that generates ASCII pictures of a cow with a message. It can also generate pictures using pre-made images of other animals, such as Tux the Penguin, the Linux mascot."
+echo
 echo "Let's take a look at another key file - ${red}ansible-hosts${reset}."
 pause
 echo ${reset}
